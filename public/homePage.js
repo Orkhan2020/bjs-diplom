eb9e127e594dcf2f3a8b4de7d54fd7ce133dae52
orkhan2020 <sherisl2017@gmail.com>
2020-10-02 23:38:54 -0700

    update

--- public/homePage.js
+++ public/homePage.js
@@ -1,82 +1,88 @@
-'use strict'
+"use strict";
 
-let logoutObject = new LogoutButton;
-logoutObject.action = function(data) {
-    ApiConnector.logout(response => {
-        response['success'] ? location.reload() : console.log(response);
-    });
+const LogButton = new LogoutButton();
+LogButton.action = (data) => {
+  ApiConnector.logout((response) => {
+    if (response.success) {
+      location.reload();
+    }
+  });
 };
 
-ApiConnector.current(response => {
-    response['success'] ? ProfileWidget.showProfile(response['data']) : console.log(response);
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+const rates = new RatesBoard();
+
+const receiveCurrencyrates = (data) => {
+  ApiConnector.getStocks((response) => {
+    rates.clearTable();
+    rates.fillTable(response.data);
+  });
+};
+receiveCurrencyrates();
+setInterval(receiveCurrencyrates, 60000);
+
+const money = new MoneyManager();
+money.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      money.setMessage(response.success, "Счет пополнен!");
+    } else money.setMessage(response.success, response.error);
+  });
+};
+money.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      money.setMessage(response.success, "Конвертация произведена!");
+    } else money.setMessage(response.success, response.error);
+  });
+};
+
+money.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      money.setMessage(response.success, "Перевод выполнен!");
+    } else money.setMessage(response.success, response.error);
+  });
+};
+
+const favorite = new FavoritesWidget();
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favorite.clearTable();
+    favorite.fillTable(response.data);
+    money.updateUsersList(response.data);
+  }
 });
 
-
-function clearAndFillStocksTable(data) {
-    let ratesBoardObject = new RatesBoard();
-    ratesBoardObject.clearTable();
-    ratesBoardObject.fillTable(data);
+favorite.addUserCallback = (data) => {
+  const parsedData = {
+    id: parseInt(data.id),
+    name: data.name,
+  };
+  ApiConnector.addUserToFavorites(parsedData, (response) => {
+    if (response.success) {
+      favorite.clearTable();
+      favorite.fillTable(response.data);
+      money.updateUsersList(response.data);
+      favorite.setMessage(true, "Пользователь добавлен!");
+    } else favorite.setMessage(false, response.error);
+  });
 };
 
-ApiConnector.getStocks(response => {
-    response['success'] ? clearAndFillStocksTable(response['data']) : console.log(response);
-});
-
-function addMoneyToWallet(object, data) {
-    ApiConnector.addMoney(data, response => {
-        response['success'] ? ProfileWidget.showProfile(response['data']) : object.setMessage(response['success'], response['error']);
-    });
-};
-
-let moneyManagerObject = new MoneyManager()
-moneyManagerObject.addMoneyCallback = function (data) {
-    data ? addMoneyToWallet(moneyManagerObject, data) : console.log(data);
-};
-
-function moneyConversation(object, data) {
-    ApiConnector.convertMoney(data, response => {
-        response['success'] ? ProfileWidget.showProfile(response['data']) : object.setMessage(response['success'], response['error']);
-    });
-};
-moneyManagerObject.conversionMoneyCallback = function (data) {
-    data ? moneyConversation(moneyManagerObject, data) : console.log(data);
-};
-
-function transferMoneyToFavorites(object, data) {
-    ApiConnector.transferMoney(data, response => {
-       response['success'] ? ProfileWidget.showProfile(response['data']) : object.setMessage(response['success'], response['error']);
-    });
-};
-
-moneyManagerObject.sendMoneyCallback = function (data) {
-    data ? transferMoneyToFavorites(moneyManagerObject, data) : console.log(data);
-};
-
-function showFavorites(object, data) {
-    object.clearTable();
-    object.fillTable(data);
-    moneyManagerObject.updateUsersList(data);
-};
-
-let favoritesObject = new FavoritesWidget()
-ApiConnector.getFavorites(response => {
-    response['success'] ? showFavorites(favoritesObject, response['data']) : favoritesObject.setMessage(response['success'], response['error']);
-});
-
-function getIntId(data) {
-    data['id'] ? data['id'] = parseInt(data['id']): data['id'];
-}
-
-favoritesObject.addUserCallback = function (data) {
-    getIntId(data);
-    ApiConnector.addUserToFavorites(data, response => {
-        response['success'] ? showFavorites(favoritesObject, response['data']) : favoritesObject.setMessage(response['success'], response['error']);
-    });
-};
-
-favoritesObject.removeUserCallback = function (data) {
-    getIntId(data);
-    ApiConnector.removeUserFromFavorites(data, response => {
-        response['success'] ? showFavorites(favoritesObject, response['data']) : favoritesObject.setMessage(response['success'], response['error']);
-    });
+favorite.removeUserCallback = (data) => {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favorite.clearTable();
+      favorite.fillTable(response.data);
+      money.updateUsersList(response.data);
+      favorite.setMessage(true, "Пользователь удален!");
+    } else favorite.setMessage(false, response.error);
+  });
 };
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,15 +1,26 @@
 "use strict";
 
-let authObject = new UserForm;
-authObject.loginFormCallback = function(data){
- ApiConnector.login(data, response =>{
-	 response['success'] ? location.reload() : authObject.setLoginErrorMessage(response['data']);
-    });
+const usForm = new UserForm();
+
+usForm.loginFormCallback = (data) => {
+  ApiConnector.login(
+    data,
+    (response) => {
+      if (response.success) {
+        location.reload();
+      } else {
+        usForm.setLoginErrorMessage(response.error);        
+      }
+    }
+  );
 };
 
-
-authObject.registerFormCallback = function(data){
- ApiConnector.register(data, response =>{
-	 response['success'] ? location.reload() : authObject.setRegisterErrorMessage(response['data']);
-    });
+usForm.registerFormCallback = (data) => {
+  ApiConnector.register(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      usForm.setRegisterErrorMessage(response.error);
+    }
+  });
 };
